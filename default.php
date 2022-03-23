5dbe71f5f7e95c3021012f1b76e92127f78799f7
Victor Martinez <victordmtz@hotmail.com>
2022-03-22 21:28:41 -0700

    Testing hook

---
+++ default.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//ES"
+ "http://www.w3.corg/TR/html4/loose.dtd">
+
+ <html lang="es">
+    <head>
+        <title>DNP FREIGHT</title>
+    </head>
+    <body>
+        <?php echo "DNP FREIGHT";?><br />
+        <?php echo "This is" . "A ne line";?>
+    </body>
+
+ </html> 
